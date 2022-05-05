edfe5205932141ba3d0a507651ebed3846a7a2cf
Eseoghene <62494879+esemoney@users.noreply.github.com>
2022-05-05 01:38:51 +0100

    commit one.

---
+++ bubblegoose pics/js/blink-script.js
@@ -0,0 +1,9 @@
+const fakeImages = document.querySelectorAll(".fake-image");
+	for (var i = 0; i < fakeImages.length; i++) {
+	  console.log('fakeImage: ', fakeImages[i]);
+	}
+
+const strip = document.getElementsByClassName(".blink-strip")
+for (var i = 0; i < strip.length; i++) {
+	
+}
